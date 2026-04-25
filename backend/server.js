82e1109ca1362fca93db4971816cc4b5f97ecac7
Ababacar Faye <Faye@192.168.1.37>
2026-04-25 00:19:11 +0000

    fix mongo connection

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("/*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
