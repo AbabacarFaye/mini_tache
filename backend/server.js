6f9c9cc6b1d336fa7f1a2f21d24abe049d6c451e
Ababacar Faye <Faye@192.168.1.37>
2026-04-25 00:33:27 +0000

    fix frontend path

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,10 @@ app.get("/", (req, res) => {
  // res.sendFile(path.join(__dirname, "public", "index.html"));
 //});
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "frontend/build")));
 
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
